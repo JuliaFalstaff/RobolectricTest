a6edae3895cce0b150e5214576468b0093bb1ce5
julia <j.kholstinina@yandex.ru>
2022-01-05 18:34:22 +0300

    add fun for actionView

--- app/src/androidTest/java/com/example/robolectrictest/recycler/MainActivityRecyclerViewFakeRepoTest.kt
+++ app/src/androidTest/java/com/example/robolectrictest/recycler/MainActivityRecyclerViewFakeRepoTest.kt
@@ -1,7 +1,10 @@
 package com.example.robolectrictest.recycler
 
+import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -10,6 +13,7 @@ import com.example.robolectrictest.MainActivity
 import com.example.robolectrictest.R
 import com.example.robolectrictest.TEST_FAKE_NUMBER_42
 import com.example.robolectrictest.view.search.SearchResultAdapter
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -66,14 +70,44 @@ class MainActivityRecyclerViewFakeRepoTest {
             )
     }
 
+    @Test
+    fun activitySearch_PerformCustomClick() {
+        loadList()
+
+        onView(withId(R.id.recyclerView))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<SearchResultAdapter.SearchResultViewHolder>(
+                        0,
+                        tapOnItemWithId(R.id.checkbox)
+                    )
+            )
+    }
+
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+
     private fun loadList() {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
     }
 
-    @After
-    fun close() {
-        scenario.close()
+    private fun tapOnItemWithId(id: Int) = object : ViewAction {
+        override fun getConstraints(): Matcher<View>? {
+            return null
+        }
+
+        override fun getDescription(): String {
+            return "Нажимаем на view с указанным id"
+        }
+
+        override fun perform(uiController: UiController?, view: View?) {
+            val v = view?.findViewById(id) as View
+            v.performClick()
+        }
     }
 }
