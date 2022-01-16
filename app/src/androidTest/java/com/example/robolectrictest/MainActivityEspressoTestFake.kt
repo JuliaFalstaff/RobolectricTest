a5e826090fa33f3b9112a8a57ec4d579b52b2357
julia <j.kholstinina@yandex.ru>
2022-01-16 14:29:23 +0300

    add test for fake repo in main activity

--- app/src/androidTest/java/com/example/robolectrictest/MainActivityEspressoTestFake.kt
+++ app/src/androidTest/java/com/example/robolectrictest/MainActivityEspressoTestFake.kt
@@ -1,24 +1,20 @@
 package com.example.robolectrictest
 
-import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase
-import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityEspressoTest {
+class MainActivityEspressoTestFake {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
 
@@ -39,7 +35,13 @@ class MainActivityEspressoTest {
 
     @Test
     fun activityButton_IsEffectiveVisible() {
-        onView(withId(R.id.toDetailsActivityButton)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.toDetailsActivityButton)).check(
+            matches(
+                withEffectiveVisibility(
+                    Visibility.VISIBLE
+                )
+            )
+        )
     }
 
     @Test
@@ -77,18 +79,7 @@ class MainActivityEspressoTest {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
-        onView(isRoot()).perform(delay())
-        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2806")))
-    }
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
 
-    private fun delay(): ViewAction? {
-        return object : ViewAction {
-            override fun getConstraints(): Matcher<View> = isRoot()
-            override fun getDescription(): String = "wait for $3 seconds"
-
-            override fun perform(uiController: UiController?, view: View?) {
-                uiController?.loopMainThreadForAtLeast(3000)
-            }
-        }
     }
 }
